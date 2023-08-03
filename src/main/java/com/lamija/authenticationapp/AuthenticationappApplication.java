628d8e96df86c3249f762da610186fca211a2753
lamijasahbegovic <lamijasahbegovic@gmail.com>
2023-08-03 18:54:04 +0200

    init commit

---
+++ src/main/java/com/lamija/authenticationapp/AuthenticationappApplication.java
@@ -0,0 +1,13 @@
+package com.lamija.authenticationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lamija/authenticationapp/AuthenticationappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamija.authenticationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
