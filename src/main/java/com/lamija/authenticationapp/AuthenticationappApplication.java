3d925844927aceecba1291c3bce97ee986690ec4
lamijasahbegovic <lamijasahbegovic@gmail.com>
2023-08-03 19:06:51 +0200

    formatting

--- src/main/java/com/lamija/authenticationapp/AuthenticationappApplication.java
+++ src/main/java/com/lamija/authenticationapp/AuthenticationappApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuthenticationappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthenticationappApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationappApplication.class, args);
+    }
 }
